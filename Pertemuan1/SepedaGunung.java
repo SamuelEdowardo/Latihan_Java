6a4bac1538983f513357fcb4c2576238d273ff21
SamuelEdowardo <samueledowardo1910@gmail.com>
2024-04-09 15:57:18 +0700

    Praktikum 1

---
+++ Pertemuan1/SepedaGunung.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan1;
+
+/**
+ *
+ * @author samuel
+ */
+class SepedaGunung extends Sepeda {
+int setSadel;
+public void setSadel(int nilaiSadel) {
+setSadel = nilaiSadel;
+
+
+System.out.println("Tinggi Sadel: "+setSadel);
+}    
+}
