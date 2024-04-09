ca1afff3393ad5c746943d648b8ebbf101508bdd
SamuelEdowardo <samueledowardo1910@gmail.com>
2024-04-09 15:55:33 +0700

    Praktikum 2

---
+++ Pertemuan2/AksiScanner.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author samuel
+ */
+public class AksiScanner {
+    public static void main(String args[])
+     
+{
+//membuat objek baru
+ClassScanner scan=new ClassScanner ();
+
+scan.inputScanner();
+System.out.println("\nNama Anda : " + scan.getnama());
+System.out.println("Nilai Anda : " + scan.rata());
+
+}
+
+}
---
+++ Pertemuan2/LatBuffered.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan2;
+import java.io.*;
+/**
+ *
+ * @author samuel
+ */
+public class LatBuffered {
+    public static void main(String args[])throws Exception
+{
+//membuat objek baru
+InputStreamReader keyreader = new InputStreamReader(System.in);
+BufferedReader input = new BufferedReader(keyreader);
+//deklarasi variabel
+String kata1, kata2;
+
+System.out.print("Masukan Kata Pertama : ");
+kata1 = input.readLine();
+System.out.print("Masukan Kata Kedua : ");
+kata2 = input.readLine();
+System.out.println("\nHasil Input String " + kata1 + " " + kata2);
+
+}
+    
+}
