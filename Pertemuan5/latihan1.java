f5209946bab774bae90f082a51b99f54425d6e5a
SamuelEdowardo <samueledowardo1910@gmail.com>
2024-04-25 14:53:44 +0700

    Praktikum 5

---
+++ Pertemuan5/latihan1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan5;
+
+/**
+ *
+ * @author samuel
+ */
+import javax.swing.*;
+
+class latihan1 {
+    public static void main(String[]args){
+        JFrame myWindow;
+        
+        myWindow=new JFrame();
+        myWindow.setSize(300,200);
+        myWindow.setTitle("My First Java Program");
+        myWindow.setVisible(true);
+    }
+}
